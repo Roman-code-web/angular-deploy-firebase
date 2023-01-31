526c81cdffc346d5af276d79e3e6c54d821e5b09
Roman-code-web <selenerg.275@gmail.com>
2023-01-31 09:00:41 -0500

    firestore

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'sesion30-deploy',
+    appId: '1:1045727276290:web:782c404b5ff1e76a71a61b',
+    storageBucket: 'sesion30-deploy.appspot.com',
+    apiKey: 'AIzaSyCDXkpEGKzWwPP2FKhpizdfxDA1Js6FThQ',
+    authDomain: 'sesion30-deploy.firebaseapp.com',
+    messagingSenderId: '1045727276290',
+  },};
